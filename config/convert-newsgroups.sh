bcd52869231eeff593d1451ed22a6556098d6524
Tristan Miller <psychonaut@nothingisreal.com>
2020-08-24 11:26:33 +0200

    Add licence headers, copyright notices, shebangs. Upgrade to GPLv3.
    Fixes #1, Fixes #5, Fixes #6.

--- config/convert-newsgroups.sh
+++ config/convert-newsgroups.sh
@@ -1,2 +1,25 @@
+#!/bin/sh
+#
+# Copyright 1999 Igor Chudov
+#
+# This file is part of WebSTUMP.
+# 
+# WebSTUMP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# WebSTUMP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with WebSTUMP.  If not, see <https://www.gnu.org/licenses/>.
+
+# This file creates a little .posted_log file in your home 
+# directory. Runs if activeated by crontab. See your crontab
+# file for more details.
+
 cat newsgroups.lst | grep -v  ^#|grep \\. \
 	| awk '{print "echo " $2 " > newsgroups/" $1 "/address.txt";}'
--- scripts/webstump.lib.pl
+++ scripts/webstump.lib.pl
@@ -1,3 +1,22 @@
+#!/usr/bin/env perl
+#
+# Copyright 1999 Igor Chudov
+#
+# This file is part of WebSTUMP.
+# 
+# WebSTUMP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# WebSTUMP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with WebSTUMP.  If not, see <https://www.gnu.org/licenses/>.
+#
 # this is a collection of library functions for stump.
 
 # error message
