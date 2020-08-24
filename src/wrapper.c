0d5f69d3b302d7cba71e902a10a66c7d6035f8a4
Tristan Miller <psychonaut@nothingisreal.com>
2020-08-24 11:45:03 +0200

    Add images README and copyright/licence headers to C files

--- src/wrapper.c
+++ src/wrapper.c
@@ -1,4 +1,23 @@
 /*
+   Copyright 1999 Igor Chudov
+
+   This file is part of WebSTUMP.
+
+   WebSTUMP is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   WebSTUMP is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with WebSTUMP.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/*
  * WebSTUMP wrapper. You have to compile this program using "make" and
  * make sure that it is installed under ../bin. It should be set up as 
  * setuid your user id. Directory referred to by webstump_home should
